542e89528e2c210890b6bf4876a99d7dd5872055
nikita-efremov <nikita.efremov1@gmail.com>
2014-10-07 18:39:41 +0400

    test commit

---
+++ sbb/data/src/main/java/ru/tsystems/tsproject/sbb/serviceImpl/PassengerServiceImpl.java
@@ -0,0 +1,15 @@
+package ru.tsystems.tsproject.sbb.serviceImpl;
+
+import ru.tsystems.tsproject.sbb.dao.StationDAO;
+import ru.tsystems.tsproject.sbb.daoImpl.StationDAOImpl;
+import ru.tsystems.tsproject.sbb.entity.Station;
+import ru.tsystems.tsproject.sbb.service.CommonService;
+
+/**
+ *
+ * PassengerService interface implementation
+ * @author  Nikita Efremov
+ * @since   1.0
+ */
+public abstract class PassengerServiceImpl implements PassengerService {
+}
